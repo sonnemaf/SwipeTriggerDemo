ae8d6c4c27addcb62105e509fc9a6798f3dc591a
sonnemaf <fons.sonnemans@reflectionit.nl>
2020-09-24 11:36:40 +0200

    Updated EventArgs to SwipeEventArgs

--- SwipeTriggerDemo/MainPage.xaml.cs
+++ SwipeTriggerDemo/MainPage.xaml.cs
@@ -1,22 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace SwipeTriggerDemo
-{
+namespace SwipeTriggerDemo {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -31,7 +18,7 @@ namespace SwipeTriggerDemo
             Grid.SetRow(border, 1);
         }
 
-        public void MoveUp(object sender, EventArgs e) {
+        public void MoveUp(object sender, SwipeEventArgs e) {
             Grid.SetRow(border, 0);
         }
     }
